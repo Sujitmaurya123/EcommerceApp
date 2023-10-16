4d4d196fb5ffaf3d2aa8d4d9eec6e9cc0572b341
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2023-10-16 17:59:39 +0530

    set backend part of ecommerceApp

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express"
+import colors from "colors"
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app=express()
+
+//middelwares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+
+app.use("/api/v1/auth",authRoutes);
+
+//rest api
+app.get('/',(req,res)=>{
+    res.send({
+        message:'Welcome to ecommerce app'
+    })
+})
+
+//PORT
+const PORT =process.env.PORT||8080;
+
+//run listen 
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${PORT}`.bgCyan.white)
+})
