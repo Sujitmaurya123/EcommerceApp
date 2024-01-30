96dbebae111fb381e6b7e9429c7f62413f5f25c2
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-01-30 23:29:06 +0530

    Make ecommerce app With MERN and using TypeScript full App realtime working

---
+++ ecommerce-frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Cart from "./pages/cart";
+const App = () => {
+  return <Router>
+
+    <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+    </Routes>
+  </Router>
+    
+  
+}
+
+export default App
