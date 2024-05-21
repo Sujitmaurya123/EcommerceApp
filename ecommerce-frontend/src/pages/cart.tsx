a741b70c54cdd8b82ef45eedd81fd1f2cca312a6
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-05-21 17:28:17 +0530

    Make home page and card component

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
+import {VscError} from "react-icons/vsc"
+
+const cartItems=[];
+const subtotal =4000;
+const tax=Math.round(subtotal*0.18);
+const shippingCharges=200;
+const discount=400;
+const total=subtotal+tax+shippingCharges;
+
 
 
 const Cart = () => {
+
+  const [couponCode,setCouponCode]=useState<string>("");
+  const [isValidCouponCode,setIsValidCouponCode]=useState<boolean>(false);
+
+  
+
   return (
-    <div>cart</div>
+    <div>
+      <main></main>
+
+      <aside>
+    <p>Subtotal: ₹{subtotal}</p>
+    <p>Shipping Charges: ₹{shippingCharges}</p>
+    <p>Tax: ₹{tax}</p>
+    <p>
+      Discount: <em> - ₹{discount}</em>
+    </p>
+    <p>
+      <b>Total:₹{total}</b>
+    </p>
+    <input type="text" value={couponCode} placeholder="Coupon Code" onChange={(e)=> setCouponCode(e.target.value)}   />
+    { couponCode &&(
+       isValidCouponCode ?(
+        <span className="green">
+          ₹{discount}off using the <code>{couponCode}</code>
+        </span>
+      ):(<span className="red" >Invalid Coupan <VscError/></span>)
+    )}
+      </aside>
+
+    </div>
   )
 }
 
-export default Cart
+export default Cart;
--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
 
+const addCartHandler =()=>{};
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div  className="home" >
+
+      <section></section>
+      <h1>Latest Products 
+        <Link  to="/search" className="findmore" >More</Link>
+      </h1>
+
+
+      <main>
+
+        <ProductCard productId="adas" 
+         name="macbook" price={500} 
+         stock={456}  handler={addCartHandler} photo="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=
+         crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjYm9va3xlbnwwfHwwfHx8MA%3D%3D" />
+      </main>
+    </div>
   )
 }
 
