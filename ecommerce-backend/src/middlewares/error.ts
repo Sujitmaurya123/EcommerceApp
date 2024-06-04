7d422bef070b5490c4aa19c163e545160e6ff211
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-06-04 13:57:00 +0530

    Make Api in order likes myorders,allorders,getSingleOrder,processOrder,deleteOrder

--- ecommerce-backend/src/middlewares/error.ts
+++ ecommerce-backend/src/middlewares/error.ts
@@ -8,7 +8,7 @@ export  const errorMiddleware=  (err:ErrorHandler,req:Request,res:Response,next:
 
     err.message ||="Internal Server Error";
     err.statusCode||=500;
-
+  if (err.name === "CastError") err.message = "Invalid ID";
     return res.status(err.statusCode).json({
         success:false,
         message:err.message,
